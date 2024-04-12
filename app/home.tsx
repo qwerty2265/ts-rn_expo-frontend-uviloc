e0246b465d414c6e10075300ec6d15bf99ae25b2
Maks <maks_mun@bk.ru>
2024-04-12 17:01:31 +0500

    Change map page to home page

---
+++ app/home.tsx
@@ -0,0 +1,12 @@
+import { COLORS } from "../constants";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const Home = () => {
+    return (
+        <SafeAreaView style={{backgroundColor: COLORS.background, flex: 1}}>
+
+        </SafeAreaView>
+    )
+}
+
+export default Home;
