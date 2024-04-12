e09c1c8c2a661bcdfdc9eb9ee9a007a9d9dfada9
Maks <maks_mun@bk.ru>
2024-04-12 18:37:31 +0500

    Create map component

--- app/home.tsx
+++ app/home.tsx
@@ -1,10 +1,11 @@
+import Map from "../components/Map/Map";
 import { COLORS } from "../constants";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const Home = () => {
     return (
         <SafeAreaView style={{backgroundColor: COLORS.background, flex: 1}}>
-
+            <Map />
         </SafeAreaView>
     )
 }
