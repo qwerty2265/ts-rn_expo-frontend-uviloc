26bc7cdd5536df740eada3590147addadb3f49c4
Maks <maks_mun@bk.ru>
2024-04-13 21:27:25 +0500

    Remove BlurView

--- components/BottomMenu/BottomMenu.tsx
+++ components/BottomMenu/BottomMenu.tsx
@@ -3,7 +3,6 @@ import Devices from "./Devices";
 import { SafeAreaView } from "react-native-safe-area-context";
 import styles from "./bottommenu.style";
 import Profile from "./Profile";
-import { BlurView } from "expo-blur";
 import BottomMenuNavigator from "./BottomMenuNavigator";
 
 const Tab = createBottomTabNavigator();
@@ -11,12 +10,10 @@ const Tab = createBottomTabNavigator();
 const BottomMenu = () => {
     return (
         <SafeAreaView style={styles.bottomMenu} >
-            <BlurView intensity={100} style={{ flex: 1 }}>
-                <BottomMenuNavigator>
-                    <Tab.Screen name='Devices' component={Devices} />
-                    <Tab.Screen name='Profile' component={Profile} />
-                </BottomMenuNavigator>
-            </BlurView>
+            <BottomMenuNavigator>
+                <Tab.Screen name='Devices' component={Devices} />
+                <Tab.Screen name='Profile' component={Profile} />
+            </BottomMenuNavigator>
         </SafeAreaView>    
     )
 }
