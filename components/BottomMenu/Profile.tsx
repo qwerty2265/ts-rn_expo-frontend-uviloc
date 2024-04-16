fd7b8fd70a02285b02e757989c0f11f97b519919
Maks <maks_mun@bk.ru>
2024-04-16 08:24:01 +0500

    Create profile form container

--- components/BottomMenu/Profile.tsx
+++ components/BottomMenu/Profile.tsx
@@ -1,34 +1,33 @@
-import { Image, StyleSheet, TextInput, View } from "react-native";
+import { View } from "react-native";
 import styles from "./bottommenu.style";
 import CustomText from "../CustomText";
-import { COLORS, FONT, icons } from "../../constants";
-import CustomTextInput from "../CustomTextInput";
+import { COLORS } from "../../constants";
 import { TouchableOpacity } from "react-native-gesture-handler";
 import LabeledTextInput from "../LabeledTextInput";
 
-
-
 const Profile = () => {
+
+
 	return (
 		<View style={styles.bottomMenuPage}>
-			<LabeledTextInput 
-				style={styles.profileForm}
-				label="Username"
-				placeholder="Enter your username..."
-			/>
+			<View style={styles.profileForm}>
+				<LabeledTextInput 
+					label="Username"
+					placeholder="Enter your username..."
+				/>
 
-			<LabeledTextInput 
-				style={styles.profileForm}
-				label="Password"
-				placeholder="Enter your password..."
-				secureTextEntry={true}
-			/>
+				<LabeledTextInput 
+					label="Password"
+					placeholder="Enter your password..."
+					secureTextEntry={true}
+				/>
 
-			<TouchableOpacity
-                style={[styles.profileForm, styles.profileFormButton]}
-            >
-                <CustomText color={COLORS.background}>Login</CustomText>
-            </TouchableOpacity>
+				<TouchableOpacity
+					style={styles.profileFormButton}
+				>
+					<CustomText color={COLORS.background}>Login</CustomText>
+				</TouchableOpacity>
+			</View>
 		</View>
 	)
 }
