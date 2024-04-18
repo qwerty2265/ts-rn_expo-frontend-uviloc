38e35e564f25ca015b0f6425fc2f406af93a9328
Maks <maks_mun@bk.ru>
2024-04-18 20:19:12 +0500

    Add user name display and sign out button to profile tab

--- components/BottomMenu/Profile.tsx
+++ components/BottomMenu/Profile.tsx
@@ -1,10 +1,37 @@
 import { View } from "react-native";
 import styles from "./bottommenu.style";
+import { useAuth } from "../../context/AuthContext";
+import CustomText from "../CustomText";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { COLORS, SIZE } from "../../constants";
+import { NavigationProp } from "@react-navigation/native";
 
-const Profile = () => {
+const Profile = ({ navigation } : { navigation : NavigationProp<any> }) => {
+	const { userData, logout } = useAuth();
+
+	const handleLogout = async () => {
+		logout();
+		navigation.navigate('auth');
+    }
+
 	return (
 		<View style={styles.bottomMenuPage}>
+			<CustomText bold>{userData?.username}</CustomText>
 
+			<TouchableOpacity
+                style={{
+					width: '50%',
+					maxWidth: 100,
+                    marginTop: SIZE.medium,
+                    backgroundColor: COLORS.accentRed,
+                    padding: 10,
+                    borderRadius: 5,
+                    alignItems: 'center',
+                }}
+                onPress={handleLogout}
+            >
+                <CustomText color={COLORS.background}>Sign out</CustomText>
+            </TouchableOpacity>
 		</View>
 	)
 }
