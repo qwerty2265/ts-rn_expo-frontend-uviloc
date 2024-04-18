e7319c58320961d50ea08fb2bb20df6774e3a597
Maks <maks_mun@bk.ru>
2024-04-18 22:19:41 +0500

    Create funciton generate random string

---
+++ utils/common.ts
@@ -0,0 +1,10 @@
+const generateRandomString = (): string => {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let result = '';
+    for (let i = 0; i < 5; i++) {
+      result += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    return result;
+};
+
+export { generateRandomString };
