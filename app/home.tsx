1175af75db0455430b53a7fb4243a87d59ee9bff
Maks <maks_mun@bk.ru>
2024-04-18 22:25:33 +0500

    Add reload on trackers change

--- app/home.tsx
+++ app/home.tsx
@@ -1,9 +1,16 @@
+import { useSelector } from "react-redux";
 import BottomMenu from "../components/BottomMenu/BottomMenu";
 import Map from "../components/Map/Map";
 import { COLORS } from "../constants";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { RootState } from "../state/store";
+import { useEffect } from "react";
 
 const Home = () => {
+    const { trackers } = useSelector((state: RootState) => state.trackers);
+
+    useEffect(() => {}, [trackers]);
+    
     return (
         <SafeAreaView style={{backgroundColor: COLORS.background, flex: 1}}>
             <Map />
