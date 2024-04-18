273e5d455b32a8f65cf05ad40045b05f620414b6
Maks <maks_mun@bk.ru>
2024-04-18 22:25:21 +0500

    Add trackers display

--- components/BottomMenu/Devices.tsx
+++ components/BottomMenu/Devices.tsx
@@ -1,16 +1,59 @@
-import { View } from "react-native";
+import { View, Text, ActivityIndicator, ScrollView } from "react-native";
 import styles from "./bottommenu.style";
-import CustomText from "../CustomText";
+import CustomText from "../CustomText"; // Предположительно это используется в другом месте вашего приложения
+import Tracker from "../Tracker/Tracker";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+import { fetchTrackersByUserToken } from "../../slices/trackerSlice";
+import { getData } from "../../utils/storage";
+import { AppDispatch, RootState } from "../../state/store";
+import { COLORS } from "../../constants";
+import { UserData } from "../../types/user";
 
 const Devices = () => {
+    const { trackers, loading, error } = useSelector((state: RootState) => state.trackers);
+    const [accessToken, setAccessToken] = useState<string | null>(null);
+    const dispatch = useDispatch<AppDispatch>();
+
+    useEffect(() => {
+        const fetchToken = async () => {
+            console.log('fetching');
+            const access_token = await getData('access_token');
+            if (typeof access_token === 'string' || access_token === null) {
+                setAccessToken(access_token);
+            } else {
+                console.error("Received non-string access token:", access_token);
+            }
+        };
+        fetchToken();
+    }, []);
+
+    useEffect(() => {
+        if (accessToken) {
+            dispatch(fetchTrackersByUserToken({ access_token: accessToken }));
+        }
+    }, [accessToken, dispatch]);
+
+    if (loading) {
+        return <ActivityIndicator style={{ flex: 1, backgroundColor: COLORS.background }} size="large" color="#0000ff" />;
+    }
+
+    if (error) {
+        return <Text>Error loading trackers: {error}</Text>;
+    }
+
     return (
-        <View style={styles.bottomMenuPage}>
-            <CustomText>
-                Devices
-            </CustomText>
-            
-        </View>
-    )
+        <ScrollView style={styles.bottomMenuPage} contentContainerStyle={{ flexGrow: 1 }}>
+            {trackers.map(tracker => (
+                <Tracker
+                key={tracker.id}
+                name={tracker.name}
+                lastTimeSeen={tracker.updated_at}
+                location={''}
+                distance={''}                />
+            ))}
+        </ScrollView>
+    );
 }
 
 export default Devices;
