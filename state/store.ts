ebe90e8eee0f2d1637ef6b7d85e5eeb670d9e5ae
Maks <maks_mun@bk.ru>
2024-04-19 21:46:11 +0500

    Add auth reducer to store config and override default redux functions

--- state/store.ts
+++ state/store.ts
@@ -1,12 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit';
 import trackerReducer from '../slices/trackerSlice';
+import authReducer from '../slices/authSlice';
+import { TypedUseSelectorHook, useDispatch as reduxUseDispatch, useSelector as reduxUseSelector  } from 'react-redux';
 
 export const store = configureStore({
     reducer: {
         trackers: trackerReducer,
+        auth: authReducer,
     },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
 
+export const useDispatch = () => reduxUseDispatch<AppDispatch>();
+export const useSelector: TypedUseSelectorHook<RootState> = reduxUseSelector;
