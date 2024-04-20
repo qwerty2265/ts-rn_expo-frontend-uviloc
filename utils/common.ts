8a328cfa72c9f65ed70eb3767dc4ed26b6b25e2e
Maks <maks_mun@bk.ru>
2024-04-21 00:20:16 +0500

    Create converting time function

--- utils/common.ts
+++ utils/common.ts
@@ -1,10 +1,28 @@
+import { TimeType } from "../types/time";
+
 const generateRandomString = (): string => {
     const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
     let result = '';
     for (let i = 0; i < 5; i++) {
-      result += characters.charAt(Math.floor(Math.random() * characters.length));
+      	result += characters.charAt(Math.floor(Math.random() * characters.length));
     }
     return result;
+};
+
+const convertUTCToLocalTime = (utcDateString: string): TimeType => {
+    const date = new Date(utcDateString);
+    const timezoneOffset = date.getTimezoneOffset() * 60000; 
+    const localDate = new Date(date.getTime() - timezoneOffset);
+
+    const time: TimeType = {
+        hours: localDate.getHours().toString().padStart(2, '0'),
+        minutes: localDate.getMinutes().toString().padStart(2, '0'),
+        day: localDate.getDate().toString().padStart(2, '0'),
+        month: (localDate.getMonth() + 1).toString().padStart(2, '0'),
+        year: localDate.getFullYear().toString()
+    };
+
+    return time;
 };
 
 export { generateRandomString };
