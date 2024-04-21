36eec46c5ef2d1c72206f06a63f5b0bcbd396842
Maks <maks_mun@bk.ru>
2024-04-21 09:43:22 +0500

    Update map styles

--- components/Map/map.style.js
+++ components/Map/map.style.js
@@ -3,7 +3,7 @@ import { Z_INDEX } from "../../constants";
 
 const styles = StyleSheet.create({
     mapContainer: {
-        height: '70%',
+        height: '50%',
         width: '100%',
         zIndex: Z_INDEX.background,
     }
