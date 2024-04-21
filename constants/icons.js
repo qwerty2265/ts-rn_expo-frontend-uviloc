1c03db40f70ec07f7356bb415b36e84bf0b710df
Maks <maks_mun@bk.ru>
2024-04-21 08:53:39 +0500

    Add tracker icon

--- constants/icons.js
+++ constants/icons.js
@@ -2,10 +2,12 @@ import devices from "../assets/icons/devices.svg";
 import profile from "../assets/icons/profile.svg";
 import plus from "../assets/icons/plus.svg";
 import qr from "../assets/icons/qr.svg";
+import tracker from "../assets/icons/tracker.svg";
 
 export default {
     devices,
     profile,
     plus,
     qr,
+    tracker,
 }
