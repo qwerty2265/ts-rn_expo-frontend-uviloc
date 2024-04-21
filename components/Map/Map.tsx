16ad72559381c34d7081da49f54112769dd44d13
Maks <maks_mun@bk.ru>
2024-04-21 16:46:29 +0500

    Create return to user location button

--- components/Map/Map.tsx
+++ components/Map/Map.tsx
@@ -1,11 +1,11 @@
+import React, { useEffect, useState, useRef } from "react";
 import MapView, { Marker } from "react-native-maps";
-import styles from "./map.style";
-import { useEffect, useState } from "react";
+import { View, TouchableOpacity } from "react-native";
 import * as Location from 'expo-location';
-import { COLORS, MAP_THEME, icons } from "../../constants";
-import { useDispatch, useSelector } from "../../state/store";
 import { CustomActivityIndicator } from "../CustomActivityIndicator";
-import CustomText from "../CustomText";
+import { useSelector } from "../../state/store";
+import styles from "./map.style";
+import { COLORS, MAP_THEME, icons } from "../../constants";
 import { convertUTCToLocalTime, formatLastTimeSeen, parseCoordinates } from "../../utils/common";
 
 interface MapRegionState {
@@ -18,9 +18,10 @@ interface MapRegionState {
 const Map = () => {
     const [mapRegion, setMapRegion] = useState<MapRegionState | null>(null);
     const [locationPermissionDenied, setLocationPermissionDenied] = useState<boolean>(false);
+    const [followsUserLocation, setFollowsUserLocation] = useState<boolean>(false);;
+    const mapRef = useRef<MapView>(null);
     const userLocation = useSelector((state) => state.location.userLocation);
     const trackers = useSelector((state) => state.trackers.trackers);
-    const dispatch = useDispatch();
 
     useEffect(() => {
         const checkPermissionAndFetchLocation = async () => {
@@ -36,12 +37,42 @@ const Map = () => {
             setMapRegion({
                 latitude: userLocation.latitude,
                 longitude: userLocation.longitude,
-                latitudeDelta: 0.04,
-                longitudeDelta: 0.05,
+                latitudeDelta: 0.004,
+                longitudeDelta: 0.005,
             });
         }
-    }, [mapRegion, userLocation]);
-    
+    }, [userLocation]);
+
+    useEffect(() => {
+        if (followsUserLocation && userLocation && mapRef.current) {
+            mapRef.current.animateToRegion({
+                latitude: userLocation.latitude,
+                longitude: userLocation.longitude,
+                latitudeDelta: 0.01,
+                longitudeDelta: 0.0,
+            }, 1000);
+        }
+    }, [followsUserLocation, userLocation]);
+
+    const handleToggleFollowUserLocation = () => {
+        setFollowsUserLocation(!followsUserLocation);
+    };
+
+    const handleRegionChangeComplete = () => {
+        if (mapRegion && followsUserLocation) animateMapToUserLocation();
+    };
+
+    const animateMapToUserLocation = () => {
+        if (mapRef.current && userLocation) {
+            mapRef.current.animateToRegion({
+                latitude: userLocation.latitude,
+                longitude: userLocation.longitude,
+                latitudeDelta: 0.01,
+                longitudeDelta: 0.02,
+            }, 1000);
+        }
+    };
+
     if (locationPermissionDenied) {
         return null;
     }
@@ -49,31 +80,48 @@ const Map = () => {
     if (!mapRegion) {
         return <CustomActivityIndicator style={{height: '60%'}} size='large' />;
     }
-    
+
     return (
-        <MapView style={styles.mapContainer}
-            region={mapRegion}
-            customMapStyle={MAP_THEME}
-            showsUserLocation={true}
-            showsMyLocationButton={true}
-            userLocationUpdateInterval={10000}
-            loadingEnabled={true}
-        >
-            {trackers.map((tracker) => (
-                <Marker
-                    key={tracker.token}
-                    coordinate={parseCoordinates(tracker.latest_geolocation.coordinates)}
-                    title={tracker.name}   
-                >
-                    <icons.tracker
-                        height={30}
-                        width={30}
-                        fill={formatLastTimeSeen(convertUTCToLocalTime(tracker.latest_geolocation.created_at)) === "Now" ? COLORS.accent : COLORS.secondary}
-                    />
-                </Marker>
-            ))}
-        </MapView>
+        <View style={styles.mapContainer}>
+            <MapView
+                ref={mapRef}
+                style={{ flex: 1 }}
+                region={mapRegion}
+                customMapStyle={MAP_THEME}
+                showsUserLocation={true}
+                userLocationUpdateInterval={10000}
+                showsMyLocationButton={true}
+                loadingEnabled={true}
+                onRegionChangeComplete={handleRegionChangeComplete}
+            >
+                {trackers.map((tracker) => (
+                    <Marker
+                        key={tracker.token}
+                        coordinate={parseCoordinates(tracker.latest_geolocation.coordinates)}
+                        title={tracker.name}   
+                    >
+                        <icons.tracker
+                            height={30}
+                            width={30}
+                            fill={formatLastTimeSeen(convertUTCToLocalTime(tracker.latest_geolocation.created_at)) === "Now" ? COLORS.accent : COLORS.secondary}
+                        />
+                    </Marker>
+                ))}
+            </MapView>
+            <TouchableOpacity 
+                style={styles.userFollowButton}
+                onPress={handleToggleFollowUserLocation}
+            >
+                <icons.arrow 
+                    style={{transform: [{rotate: followsUserLocation ? '0deg' : '45deg'}]}} 
+                    height={20} 
+                    width={20} 
+                    fill={COLORS.text} 
+                    stroke={COLORS.text}
+                />
+            </TouchableOpacity>
+        </View>
     )
 }
 
-export default Map;
+export default Map
