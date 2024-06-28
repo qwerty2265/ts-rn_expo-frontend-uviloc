61ea68681f28ded27efe2cb0db2a41b517da9eff
Maks <maks_mun@bk.ru>
2024-06-28 21:30:14 +0500

    Create tracker details page

---
+++ app/tracker-details/[id].tsx
@@ -0,0 +1,15 @@
+import { View } from "react-native";
+import { COLORS } from "../../constants";
+import styles from "./trackerdetails.style";
+
+const TrackerDetails = () => {
+    return (
+        <View style={styles.trackerDetailsMenu}>
+            <View style={styles.trackerDetailsMenuPage}>
+
+            </View>
+        </View>
+    )
+}
+
+export default TrackerDetails;
