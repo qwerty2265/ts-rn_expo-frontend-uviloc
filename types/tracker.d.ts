c60c5c303d4e4824306563de04ed524d2180f41e
Maks <maks_mun@bk.ru>
2024-07-06 11:22:53 +0500

    Update tracker type

--- types/tracker.d.ts
+++ types/tracker.d.ts
@@ -4,6 +4,12 @@ interface TrackerType {
     token: string;
     sim_phone_number: string | null;
     name: string;
+    battery_percentage: number | null;
+    signal_strength: number | null;
+    net_standart: string | null;
+    isp: string | null;
+    gps_state: boolean | null;
+    speed_kmh: number | null;
     created_at: string;
     updated_at: string;
     latest_geolocation: TrackerGeolocationType;
